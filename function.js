0fc7592b7dcba909fd13f65745b7b4a1f8edc2a7
Bharath-Kumar11 <pbhkr11@gmail.com>
2024-10-14 14:44:50 +0530

    added

---
+++ function.js
@@ -0,0 +1,7 @@
+function great(name)
+{
+    console.log("hai"+name);
+}
+greet("krishna");
+greet("prasad");
+function greet1(firstname,lastname)
---
+++ object1.js
@@ -0,0 +1,15 @@
+
+//object literals
+let person=
+{
+ name:"mosh",
+age:12
+};
+console.log(person);
+//1.Dot notation
+person.name="Shreya";
+console.log(person.name);
+//2.Bracket Notation
+person['name']="Asin";
+console.log(person.name)
+
